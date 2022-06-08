6487dc2698666a054aef64629bca30056ccf2b95
Matheus Felix <matheusfelix.amorim@hotmail.com>
2022-06-08 17:04:06 -0300

    master

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -4,4 +4,15 @@ export const Container = styled.div`
   background-color: #24282F;
   min-height: 100vh;
   color: #FFF;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Map = styled.div`
+  width: 480px;
+  height: 480px;
+  background-image: url('/assets/map.png');
+  background-size: 100%;
+  background-position: left top;
 `;
