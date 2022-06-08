8da0fed7d771c91fea3cfedcc65356ae79d4d845
Matheus Felix <matheusfelix.amorim@hotmail.com>
2022-06-08 17:11:16 -0300

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import * as C from './App.styles';
+import { Character } from './components/Character';
 
 const App = () => {
   return (
     <C.Container>
       <C.Map>
-
+        <Character x={30} y={30} />
       </C.Map>
     </C.Container>
   );
